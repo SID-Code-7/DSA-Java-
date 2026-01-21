7c3dc013078ccf29e2cfb4d51b83aa59bdd8cd4f
SID-CODE-7 <sidsoni5454@gmail.com>
2026-01-21 11:24:40 +0530

    string

--- strings/palindrom.java
+++ strings/palindrom.java
@@ -3,10 +3,11 @@ public class palindrom {
     public static void main(String[] args) {
         String str = "racecar";
         int n = str.length();
-        boolean flag=false;
+        boolean flag=true;
         for(int i=0;i<n/2; i++){
-            if(str.charAt(i)==str.charAt(n-1-i)){
-                flag=true;
+            if(str.charAt(i) !=str.charAt(n-1-i)){
+                flag=false;
+                break;
             }
         }
         if(flag==true){
