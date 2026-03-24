111456f4ca1713128e8c39260710ff0205890aa0
SID-CODE-7 <sidsoni5454@gmail.com>
2026-03-24 07:10:28 +0530

    backtrack

---
+++ backtracking/substr.java
@@ -0,0 +1,25 @@
+package backtracking;
+
+public class substr {
+    public static void choices(String str,String ans,int i){
+        //base case
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("null");
+            }
+            else{
+                System.out.println(ans);
+            }
+            return;
+        }
+        //kaam
+        //yes choide
+        choices(str, ans+str.charAt(i), i+1);
+        //no choices 
+        choices(str, ans, i+1);
+    }
+   public static void main(String[] args) {
+    String str = "abc";
+    choices(str, "", 0);
+   } 
+}
